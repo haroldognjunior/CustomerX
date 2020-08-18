c95e0952acbac8d24398cb57709fe40fe03cff65
Júnior Nogueira <haroldognjunior@gmail.com>
2020-08-18 01:16:52 -0300

    redux

---
+++ client/src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+
+const mainReducer = combineReducers({
+    
+
+});
+
+export default mainReducer;
---
+++ client/src/reducers/userReducers.js
@@ -0,0 +1,14 @@
+import {
+
+} from "../constants/userConstants";
+
+const initialState = {
+   
+  };
+
+export default function usuario(state = initialState, action) {
+    switch (action.type) {
+        default:
+      return state;
+    }
+}
